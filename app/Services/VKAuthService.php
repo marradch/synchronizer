5dbaf3c24c275c21c114c65cea29a0d1e7ee3ea0
Radchenko Marina <mar.radchenko@gmail.com>
2019-10-23 11:33:14 +0300

    refactor: make refactor for vk auth service
    refactor for task #1

--- app/Services/VKAuthService.php
+++ app/Services/VKAuthService.php
@@ -17,7 +17,7 @@ class VKAuthService
 	private $groupId;
 	private $redirectURI;
 	private $tokenModel;
-	
+
     /**
      * Create a new instance.
      *
@@ -28,95 +28,83 @@ class VKAuthService
         $this->appId = env('VK_APP_ID', null);
 		$this->appSecret = env('VK_APP_SECRET', null);
 		$this->groupId = env('VK_GROUP_ID', null);
-		$protocol = strtolower(substr($_SERVER['SERVER_PROTOCOL'],0,strpos( $_SERVER['SERVER_PROTOCOL'],'/'))).'://';
-		//$redirect_uri = $protocol.$_SERVER['HTTP_HOST'].'/authRedirect'; 
-		$this->redirectURI = $protocol.'e967cf16.ngrok.io'.'/authRedirect';
-		$this->token = new Token();		
+        $this->redirectURI = route('vk.redirect_uri');
+		$this->tokenModel = new Token();
     }
-	
+
 	public function buildLoginUrl()
 	{
 		$oauth = new VKOAuth();
-		
+
 		$scope = [ VKOAuthUserScope::MARKET ];
 		$state = 'secret_state_code';
-		
+
 		if(!$this->checkOfflineToken()) {
 			$scope[] = VKOAuthUserScope::OFFLINE;
-		}			
+		}
 
 		$loginUrl = $oauth->getAuthorizeUrl(
-			VKOAuthResponseType::CODE, 
+			VKOAuthResponseType::CODE,
 			$this->appId,
-			$this->redirectURI, 
-			VKOAuthDisplay::PAGE, 
-			$scope, 
+			$this->redirectURI,
+			VKOAuthDisplay::PAGE,
+			$scope,
 			$state
 		);
-		
+
 		return $loginUrl;
 	}
-	
+
 	public function processRedirect($code)
 	{
 		$oauth = new VKOAuth();
 
-		$response = $oauth->getAccessToken($this->appId, $this->appSecret, $this->redirectURI, $code);								
-		
-		$access_token = $response['access_token'];		
+		$response = $oauth->getAccessToken($this->appId, $this->appSecret, $this->redirectURI, $code);
+		$access_token = $response['access_token'];
 		$user_id = $response['user_id'];
 		$expires_in = $response['expires_in'];
-		
+
 		$vk = new VKApiClient();
-		
 		$response = $vk->groups()->get($access_token, array(
 			'user_id' => $user_id,
 			'filter' => 'admin',
-		));			
+		));
 
 		if($response['items'] && in_array($this->groupId, $response['items'])) {
-			
 			if($expires_in == 0) {
 				$token = new Token();
 				$token->token = $access_token;
 				$token->save();
 			}
-			
-			$response = $vk->users()->get($access_token);				
-			
+
+			$response = $vk->users()->get($access_token);
 			$authData = [
 				'access_token' => $access_token,
 				'user_id' => $user_id,
 				'full_name' => $response[0]['first_name'].' '.$response[0]['last_name'],
 			];
-			
 			session(['authData' => $authData]);
-			
+
 			return true;
 		}
-		
-		return false;			
+
+		return false;
 	}
-	
+
 	private function checkOfflineToken()
 	{
-		$tokenItem = $this->token->first();
+		$tokenItem = $this->tokenModel->first();
 		if($tokenItem){
-			
 			$vk = new VKApiClient();
-		
 			try{
-				$response = $vk->users()->get($tokenItem->token);
-				
+				$vk->users()->get($tokenItem->token);
+
 				return true;
-				
 			} catch(VKApiAuthException $e) {
 				$tokenItem->delete();
 			}
-
 		}
-		
+
 		return false;
 	}
-	
 }
