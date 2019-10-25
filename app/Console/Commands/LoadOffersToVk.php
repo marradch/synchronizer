91070d6c650c45452e4b5fd3537acee204454b91
Radchenko Marina <mar.radchenko@gmail.com>
2019-10-25 20:13:18 +0300

    feat: add console command for loading to vk
    for task #2

---
+++ app/Console/Commands/LoadOffersToVk.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Services\VKSynchronizerService;
+
+class LoadOffersToVk extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'synchronizer:load-offers-to-vk';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Load offers and categories to vk from database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        (new VKSynchronizerService())->loadAllToVK();
+    }
+}
